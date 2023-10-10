c9185ec0c0c63474006c56daa04243ed3c8c6aa9
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-10-10 18:28:29 +0000

    Add home test

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,8 +1,42 @@
+import { Button } from "../../components/Button";
+import { Heading } from "../../components/Heading";
+import { Text } from "../../components/Text";
+import { TextInput } from "../../components/TextInput";
+
+import { EnvelopeClosedIcon } from "@radix-ui/react-icons";
+
+import logo from "../../../public/guara-logo.png";
+
 function Home() {
+    //
     return (
-        <div>
-            <h1>Bem-vindo à Guará Júnior</h1>
-            <p>Seu sistema de gerenciamento empresarial.</p>
+        <div className="flex flex-col items-end h-full w-full">
+            <div className="flex justify-center flex-col items-center h-full bg-green-300">
+                {
+                /* Botar minimo depois */
+                }
+                <div className="flex justify-center items-center flex-col px-32">
+                    <img src={logo} className="h-[312px] translate-x-6 w-fit"></img>
+                    <Heading size="lg" className="font-normal">GuaráRP</Heading>
+                    <Heading size="md">Empresas md Heading 28</Heading>
+                    <Heading>Empresas md Heading 28</Heading>
+                    <Heading size="sm">Empresas sm Heading 20</Heading>
+                    <Text size="md">Empresas md Text 18</Text>
+                    <Text>Empresas md Text 18</Text>
+                    <Text size="sm">Empresas sm Text 16</Text>
+                    <Text size="xm">Empresas xm Text 14</Text>
+                    <TextInput.Root>
+                        <TextInput.Icon>
+                            <EnvelopeClosedIcon/>
+                        </TextInput.Icon>
+                        <TextInput.Input placeholder="Insira seu texto" />
+                        <TextInput.Icon>
+                            <EnvelopeClosedIcon/>
+                        </TextInput.Icon>
+                    </TextInput.Root>
+                    <Button className="bg-emerald-500 text-zinc-50">Botão</Button>
+                </div>
+            </div>
         </div>
     );
 }
