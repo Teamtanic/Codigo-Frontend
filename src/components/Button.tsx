bd47ca71d06bbf46470cbac15fd3f87c0d998375
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-10-10 18:28:03 +0000

    Add button pattern

---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import { clsx } from "clsx";
+import { Slot } from "@radix-ui/react-slot";
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  asChild?: boolean;
+  className?: string;
+}
+
+export function Button({
+  children,
+  asChild,
+  className,
+  ...props
+}: ButtonProps) {
+  const Component = asChild ? Slot : "button";
+
+  return (
+    <>
+      <Component
+        className={clsx(
+          "font-poppins py-3 px-4 bg-emerald-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-emerald-400 focus:ring-2 ring-white",
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </Component>
+    </>
+  );
+}
