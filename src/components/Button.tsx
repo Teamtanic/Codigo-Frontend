6afb6e756a5d7ac45380f7abc9b0ef31e5be9484
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-10-12 20:09:47 +0000

    Alter pattern color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -20,7 +20,7 @@ export function Button({
     <>
       <Component
         className={clsx(
-          "font-poppins py-3 px-4 bg-emerald-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-emerald-400 focus:ring-2 ring-white",
+          "font-poppins py-3 px-4 bg-emerald-500 rounded font-semibold text-gray-900 text-sm w-full transition-colors hover:bg-emerald-400 focus:ring-2 ring-white",
           className
         )}
         {...props}
