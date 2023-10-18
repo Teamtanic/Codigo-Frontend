29fa18eadfdf584c2fac92185231e2e7bb75c69c
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-10-18 16:07:11 +0000

    Add icon to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,32 +1,41 @@
 import { clsx } from "clsx";
 import { Slot } from "@radix-ui/react-slot";
 import { ButtonHTMLAttributes, ReactNode } from "react";
+import { Text } from "./Text";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
   className?: string;
+  icon?: ReactNode;
+  iconPosition?: "left" | "right"; // Nova propriedade para controlar a posição do ícone
 }
 
 export function Button({
   children,
   asChild,
+  iconPosition = "left", // Posição padrão do ícone é à esquerda
+  icon,
   className,
   ...props
 }: ButtonProps) {
   const Component = asChild ? Slot : "button";
 
   return (
-    <>
-      <Component
-        className={clsx(
-          "font-poppins py-3 px-4 bg-emerald-500 rounded font-semibold text-gray-900 text-sm w-full transition-colors hover:bg-emerald-400 focus:ring-2 ring-white",
-          className
-        )}
-        {...props}
-      >
-        {children}
-      </Component>
-    </>
+    <Component
+      className={clsx(
+        "flex items-center justify-center py-3 px-4 bg-emerald-500 rounded font-semibold text-gray-900 text-sm w-full transition-colors hover:bg-emerald-400 focus:ring-2 ring-white",
+        className
+      )}
+      {...props}
+    >
+      {iconPosition === "left" && (
+          <Slot className="w-6 h-6 text-gray-900">{icon}</Slot>
+      )}
+      <Text className={`text-center flex-1 ${icon && iconPosition === "left" ? "-translate-x-3" : iconPosition === "right" ? "translate-x-3" : ""}`}>{children}</Text>
+      {iconPosition === "right" && (
+          <Slot className="w-6 h-6 text-gray-900">{icon}</Slot>
+      )}
+    </Component>
   );
 }
