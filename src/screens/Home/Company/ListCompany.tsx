e7e973b86eacb53f3337021a4aa14a5169badf7e
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-10-25 15:20:06 +0000

    Add copy text

--- src/screens/Home/Company/ListCompany.tsx
+++ src/screens/Home/Company/ListCompany.tsx
@@ -5,21 +5,39 @@ import { Heading } from "../../../components/Heading";
 import { Navbar } from "../../../components/Navbar";
 import { Text } from "../../../components/Text";
 import { TextInput } from "../../../components/TextInput";
+import { Button } from "../../../components/Button";
+import { useState } from "react";
+
 
 export function ListCompany() {
+    const [isDialogVisible, setIsDialogVisible] = useState(false);
+
     var empresas = [
-        { id: 231321, nome: 'MCDONALDS', relacao: 'fornececedor', codigo: 13233123323 },
-        { id: 76575677657567, nome: 'Burger King', relacao: 'fornececedor', codigo: '1323dawdwada3123323' },
-        { id: 235431321, nome: 'Vivara', relacao: 'fornececedor', codigo: 13233123323 },
-        { id: 231398721, nome: 'Posto Ipiranga', relacao: 'fornececedor', codigo: '1323312332sadada3' },
-        { id: 876231321, nome: 'Microsoft', relacao: 'fornececedor', codigo: 13233123323 }
+        { id: 231321, nome: 'MCDONALDS', relacao: 'fornecedor', codigo: 13233123323 },
+        { id: 1, nome: 'Burger King', relacao: 'fornecedor', codigo: '1323dawdwada3123323' },
+        { id: 235431321, nome: 'Vivara', relacao: 'fornecedor', codigo: 13233123323 },
+        { id: 231398721, nome: 'Posto Ipiranga', relacao: 'cliente', codigo: '1323312332sadada3' },
+        { id: 876231321, nome: 'Microsoft', relacao: 'fornecedor', codigo: 13233123323 },
+        { id: 87627631321, nome: 'Microsoft', relacao: 'fornecedor', codigo: 13233123323 }
     ];
 
+    function copyText(event: React.MouseEvent<HTMLElement>) {
+        const { target } = event;
+        
+        if (target instanceof HTMLElement) {
+          const textToCopy = target.textContent;
+          
+          if (textToCopy) {
+            navigator.clipboard.writeText(textToCopy);
 
-    function copyText (event: React.MouseEvent<HTMLElement>) {
-        console.log(event.relatedTarget)
-        //navigator.clipboard.writeText(event.target);
-    }
+            setIsDialogVisible(true);
+
+            setTimeout(() => {
+                setIsDialogVisible(false);
+              }, 3000);
+          }
+        }
+      }
 
 
     return (
@@ -36,7 +54,7 @@ export function ListCompany() {
                     </Card>
 
                     <div className="mt-10">
-                        <table className="w-full">
+                        <table className="w-full ">
                             <thead>
                                 <Card className="w-full py-3 px-4 rounded-bl-none items-center">
                                     <tr className="w-full grid grid-cols-9 justify-start gap-3">
@@ -63,21 +81,23 @@ export function ListCompany() {
                                     return (
                                         <Card key={empresa.id} className="w-full py-3 px-4 mt-4">
                                             <tr className="grid px-4 grid-cols-9 items-center overflow-hidden gap-3">
-                                                <td className="col-span-1 flex justify-start">
-                                                    <Heading className="!text-gray-800 truncate">{empresa.id}</Heading>
+                                                <td className="col-span-1 flex justify-start truncate">
+                                                    <Button className="bg-transparent !justify-start hover:bg-gray-200" title='Clique para copiar' textStyle="!text-gray-800 truncate font-semibold !text-left" onClick={copyText}>
+                                                        {empresa.id}
+                                                    </Button>
                                                 </td>
                                                 <td className="col-span-3 flex justify-start">
-                                                    <Heading className="!text-gray-800 max-h-10 truncate">{empresa.nome}</Heading>
+                                                    <Text className="!text-gray-800 max-h-10 truncate font-semibold">{empresa.nome}</Text>
                                                 </td>
                                                 <td className="col-span-2 flex justify-start">
-                                                    <Heading className="m-auto">
-                                                        <Card className="!bg-blue-950 w-full flex items-center justify-center">
-                                                            <Text className="!text-gray-100 ">{empresa.relacao}</Text>
+                                                    <Text className="m-auto">
+                                                        <Card className={`${empresa.relacao == "fornecedor" ? "!bg-blue-950 ": "!bg-emerald-600"} w-full flex items-center justify-center`}>
+                                                            <Text size="xm" className="!text-gray-100 font-semibold">{empresa.relacao}</Text>
                                                         </Card>
-                                                    </Heading>
+                                                    </Text>
                                                 </td>
-                                                <td className="col-span-2 flex justify-start" onClick={copyText}>
-                                                    <Heading className="!text-gray-800 truncate">{empresa.codigo}</Heading>
+                                                <td className="col-span-2 flex justify-start">
+                                                    <Text className="!text-gray-800 truncate font-semibold">{empresa.codigo}</Text>
                                                 </td>
                                                 <td className="col-span-1 flex justify-start">
                                                     <DotsThreeOutlineVertical className="!text-gray-800 truncate ml-auto mr-0" size={32} />
@@ -86,6 +106,11 @@ export function ListCompany() {
                                         </Card>
                                     )
                                 })}
+                                {isDialogVisible && (
+                                    <div className="fixed w-fit bottom-5 right-5 bg-white p-3 rounded shadow border border-gray-300">
+                                        <Text className="text-gray-900">Copiado!</Text>
+                                    </div>
+                                )}
                             </tbody>
                         </table>
                     </div>
