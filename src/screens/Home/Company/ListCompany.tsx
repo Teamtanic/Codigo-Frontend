5a4c625539c4ed4b1499ff189183ead5b3597eb6
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-10-28 17:20:05 +0000

    Add table responsive

--- src/screens/Home/Company/ListCompany.tsx
+++ src/screens/Home/Company/ListCompany.tsx
@@ -13,31 +13,64 @@ export function ListCompany() {
     const [isDialogVisible, setIsDialogVisible] = useState(false);
 
     var empresas = [
-        { id: 231321, nome: 'MCDONALDS', relacao: 'fornecedor', codigo: 13233123323 },
-        { id: 1, nome: 'Burger King', relacao: 'fornecedor', codigo: '1323dawdwada3123323' },
-        { id: 235431321, nome: 'Vivara', relacao: 'fornecedor', codigo: 13233123323 },
-        { id: 231398721, nome: 'Posto Ipiranga', relacao: 'cliente', codigo: '1323312332sadada3' },
-        { id: 876231321, nome: 'Microsoft', relacao: 'fornecedor', codigo: 13233123323 },
-        { id: 87627631321, nome: 'Microsoft', relacao: 'fornecedor', codigo: 13233123323 }
+        { id: "d5d6be6e-d332-41be-b601-8bd2ae4e6935", nome: 'MCDONALDS', relacao: 'fornecedor', codigo: "61556481000100" },
+        { id: "37514f28-11af-4ef5-aae9-6054d8250fdd", nome: 'Burger King', relacao: 'fornecedor', codigo: '48820178000105' },
+        { id: "50ef8007-a694-4617-861e-98ac87ca2a31", nome: 'Vivara', relacao: 'fornecedor', codigo: "54365277600" },
+        { id: "bfdfe8f5-500b-4b7a-83ec-0fbb33fb63b6", nome: 'Posto Ipiranga', relacao: 'cliente', codigo: '48820178000105' },
+        { id: "7ba0b55a-0c52-4d13-bb0f-841121582f35", nome: 'Microsoft', relacao: 'fornecedor', codigo: "85445832023" },
+        { id: "4d19e455-9725-435f-96f4-c0d0a684a202", nome: 'Microsoft', relacao: 'fornecedor', codigo: "48820178000105" }
     ];
 
-    function copyText(event: React.MouseEvent<HTMLElement>) {
-        const { target } = event;
-        
-        if (target instanceof HTMLElement) {
-          const textToCopy = target.textContent;
-          
-          if (textToCopy) {
-            navigator.clipboard.writeText(textToCopy);
-
-            setIsDialogVisible(true);
-
-            setTimeout(() => {
-                setIsDialogVisible(false);
-              }, 3000);
-          }
+    const codeMask = (code: string) => {
+        if (code.length === 11) {
+            return code
+                .replace(/\D/g, '') // substitui qualquer caracter que nao seja numero por nada
+                .replace(/(\d{3})(\d)/, '$1.$2') // captura 2 grupos de numero o primeiro de 3 e o segundo de 1, apos capturar o primeiro grupo ele adiciona um ponto antes do segundo grupo de numero
+                .replace(/(\d{3})(\d)/, '$1.$2')
+                .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+                .replace(/(-\d{2})\d+?$/, '$1') // captura 2 numeros seguidos de um traço e não deixa ser digitado mais nada
         }
-      }
+
+        if (code.length === 14) {
+            return code
+                .replace(/\D+/g, '') // não deixa ser digitado nenhuma letra
+                .replace(/(\d{2})(\d)/, '$1.$2') // captura 2 grupos de número o primeiro com 2 digitos e o segundo de com 3 digitos, apos capturar o primeiro grupo ele adiciona um ponto antes do segundo grupo de número
+                .replace(/(\d{3})(\d)/, '$1.$2')
+                .replace(/(\d{3})(\d)/, '$1/$2') // captura 2 grupos de número o primeiro e o segundo com 3 digitos, separados por /
+                .replace(/(\d{4})(\d)/, '$1-$2')
+                .replace(/(-\d{2})\d+?$/, '$1') // captura os dois últimos 2 números, com um - antes dos dois números
+        }
+
+        return ''
+    }
+
+    function formatUUID(inputString: string) {
+        if (inputString.length === 36) {
+            const startChars = inputString.slice(0, 5);
+            const endChars = inputString.slice(-5);
+            return `${startChars}...${endChars}`;
+        } else {
+            return inputString;
+        }
+    }
+
+    function copyText(event: React.MouseEvent<HTMLElement>, id: string) {
+        const { target } = event;
+
+        if (target instanceof HTMLElement) {
+            const textToCopy = id;
+
+            if (textToCopy) {
+                navigator.clipboard.writeText(textToCopy);
+
+                setIsDialogVisible(true);
+
+                setTimeout(() => {
+                    setIsDialogVisible(false);
+                }, 3000);
+            }
+        }
+    }
 
 
     return (
@@ -57,23 +90,23 @@ export function ListCompany() {
                     </Card>
 
                     <div className="mt-10">
-                        <table className="w-full ">
-                            <thead>
-                                <Card className="w-full py-3 px-4 rounded-bl-none items-center">
-                                    <tr className="w-full grid grid-cols-9 justify-start gap-3">
-                                        <th className="col-span-1 max-md:col-span-2 flex justify-start">
+                        <table className="w-full">
+                            <thead >
+                                <Card asChild  >
+                                    <tr className="w-full py-3 text-left border-b-8 border-white dark:border-gray-700">
+                                        <th className="py-3 px-4 rounded-bl-none rounded-tl-2xl items-center border-r-4 border-gray-700">
                                             <Heading className="!text-gray-800">ID</Heading>
                                         </th>
-                                        <th className="col-span-3 max-md:col-span-4 flex justify-start">
-                                            <Heading className="!text-gray-800">| Nome</Heading>
+                                        <th className="px-4 w-96 border-r-4 border-gray-700">
+                                            <Heading className="!text-gray-800">Nome</Heading>
                                         </th>
-                                        <th className="col-span-2 max-md:col-span-3 flex justify-center">
-                                            <Heading className="!text-gray-800">| Relação</Heading>
+                                        <th className="px-4 w-48 md:border-r-4 md:border-gray-700 max-md:rounded-r-2xl">
+                                            <Heading className="!text-gray-800">Relação</Heading>
                                         </th>
-                                        <th className="col-span-2 max-md:hidden flex justify-start">
-                                            <Heading className="!text-gray-800">| Código</Heading>
+                                        <th className="px-4 max-md:hidden">
+                                            <Heading className="!text-gray-800">Código</Heading>
                                         </th>
-                                        <th className="col-span-1 max-md:col-span-0 flex justify-start max-md:hidden">
+                                        <th className="px-4 max-md:hidden rounded-r-2xl">
                                         </th>
                                     </tr>
                                 </Card>
@@ -82,28 +115,32 @@ export function ListCompany() {
                             <tbody>
                                 {empresas.map(empresa => {
                                     return (
-                                        <Card key={empresa.id} className="w-full py-3 px-4 mt-4 shadow-md">
-                                            <tr className="grid px-4 max-md:px-1 max-md:gap-1 grid-cols-9 items-center overflow-hidden gap-3">
-                                                <td className="col-span-1 max-md:col-span-2 flex justify-start truncate">
-                                                    <Button className="bg-transparent !px-1 !justify-start hover:bg-gray-200" title='Clique para copiar' textSize="xm" textStyle="!text-gray-800 truncate font-semibold !text-left" onClick={copyText}>
-                                                        {empresa.id}
+                                        <Card asChild key={empresa.id} className="">
+                                            <tr className="h-16 border-b-8 border-white dark:border-gray-700">
+                                                <td className="w-32 rounded-l-xl">
+                                                    <Button className="bg-transparent !px-1 !justify-start hover:bg-gray-200" title='Clique para copiar' textSize="xm" textStyle="!text-gray-800 truncate font-semibold" onClick={(event) => copyText(event, empresa.id)}>
+                                                        {formatUUID(empresa.id)}
                                                     </Button>
                                                 </td>
-                                                <td className="col-span-3 max-md:col-span-4 flex justify-start">
+                                                <td className="px-2">
                                                     <Text className="!text-gray-800 max-h-10 truncate font-semibold">{empresa.nome}</Text>
                                                 </td>
-                                                <td className="col-span-2 max-md:col-span-3 flex justify-start">
-                                                    <Text className="m-auto">
-                                                        <Card className={`${empresa.relacao == "fornecedor" ? "!bg-blue-950 ": "!bg-emerald-600"} w-full flex items-center justify-center`}>
-                                                            <Text size="xm" className="!text-gray-100 font-semibold">{empresa.relacao}</Text>
-                                                        </Card>
-                                                    </Text>
+                                                <td className="px-2 max-md:rounded-r-xl">
+                                                    <Card className={`${empresa.relacao == "fornecedor" ? "!bg-blue-950 " : "!bg-emerald-600"} w-fit flex items-center justify-center`}>
+                                                        <Text size="xm" className="!text-gray-100 font-semibold">{empresa.relacao}</Text>
+                                                    </Card>
                                                 </td>
-                                                <td className="col-span-2 flex justify-start max-md:hidden">
-                                                    <Text className="!text-gray-800 truncate font-semibold">{empresa.codigo}</Text>
+                                                <td className="px-2 max-md:hidden">
+                                                    <Card className="!bg-sky-300">
+                                                        <Text className="!text-gray-800 truncate font-semibold">
+                                                            {empresa.codigo.length === 11 ? 'CPF: ' : empresa.codigo.length === 14 ? "CNPJ: " : "Não consta"}{codeMask(empresa.codigo)}
+                                                        </Text>
+                                                    </Card>
                                                 </td>
-                                                <td className="col-span-1 flex justify-start max-md:hidden">
-                                                    <DotsThreeOutlineVertical className="!text-gray-800 truncate ml-auto mr-0" size={32} />
+                                                <td className="px-6 max-md:hidden rounded-r-xl">
+                                                    <div className="hover:bg-gray-200 h-full p-1 w-fit rounded-full mr-0 ml-auto">
+                                                        <DotsThreeOutlineVertical className="!text-gray-800 truncate ml-auto mr-0" size={32} />
+                                                    </div>
                                                 </td>
                                             </tr>
                                         </Card>
