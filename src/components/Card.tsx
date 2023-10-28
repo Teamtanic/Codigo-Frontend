fd534a9bbdadf68a2829e33f4458c032e1ce0a98
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-10-28 17:18:51 +0000

    Add asChild option

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,19 @@
+import { Slot } from "@radix-ui/react-slot";
 import { clsx } from "clsx";
 import { ReactNode } from "react";
 
 export interface CardProps {
     children: ReactNode;
     className?: string;
+    asChild?: boolean;
 }
 
-export function Card({children, className}:CardProps){
+export function Card({children, className, asChild}:CardProps){
+    const Component = asChild ? Slot : "div";
 
     return(
-        <div className={clsx("bg-zinc-100 w-fit h-fit rounded-xl p-2", className)}>
+        <Component className={clsx("bg-zinc-100 w-fit h-fit rounded-xl p-2", className)}>
             {children}
-        </div>
+        </Component>
     )
 }
