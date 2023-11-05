dfdf8ccae3a8740b91621714b9d259177b9f13ff
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-11-05 01:53:39 +0000

    Add list role

---
+++ src/screens/RH/Role/ListRole.tsx
@@ -0,0 +1,53 @@
+import { Card } from "../../../components/Card";
+import { Container } from "../../../components/Container";
+import { Heading } from "../../../components/Heading";
+import { Navbar } from "../../../components/Navbar";
+import { TextInput } from "../../../components/TextInput";
+import { MagnifyingGlass } from 'phosphor-react';
+import { CardModal } from "../../../components/CardModal";
+import { TableListRole } from "./TableListRole";
+
+export function ListRoles() {
+
+    return (
+        <Container>
+            <Navbar />
+            <div className="w-full flex py-6 p-24 max-md:p-6 flex-col">
+                <Heading size="lg">Cargos</Heading>
+
+                <div className="w-full mt-3">
+                    <Card className="w-full py-3 px-4">
+                        <TextInput.Root colorLabel="text-gray-900">
+                            <TextInput.Icon>
+                                <MagnifyingGlass />
+                            </TextInput.Icon>
+                            <TextInput.Input id="nome" type="text" placeholder="Pesquisar..." />
+                        </TextInput.Root>
+                    </Card>
+
+                    <div className="mt-10">
+                        <TableListRole />
+
+                        <CardModal title="Cadastro de Produto" action="Adicionar" triggerStyle="fixed w-fit bottom-5 right-8 rounded shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] !text-gray-100">
+                            <div className="flex flex-col w-full max-md:px-12 md:px-24 mb-12 gap-4">
+                                <TextInput.Root labelFor="product" labelText="Produto">
+                                    <TextInput.Input id="product" type="text" placeholder="Digite o produto..." />
+                                </TextInput.Root>
+                                <TextInput.Root labelFor="quantity" labelText="Quantidade">
+                                    <TextInput.Input id="quantity" type="number" min="0" placeholder="Digite a quantidade..." />
+                                </TextInput.Root>
+                                <TextInput.Root labelFor="supplier" labelText="Fornecedor">
+                                    <TextInput.Input id="supplier" type="text" placeholder="Informe o fornecedor..." />
+                                </TextInput.Root>
+                                <TextInput.Root labelFor="price" labelText="Preço">
+                                    <TextInput.Input id="price" type="text" placeholder="Informe o preço..." />
+                                </TextInput.Root>
+                            </div>
+                        </CardModal>
+                    </div>
+                </div>
+            </div>
+
+        </Container>
+    )
+}
