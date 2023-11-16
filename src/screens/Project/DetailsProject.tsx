1c908f89c5e5674d82a836c320e0f599439228a1
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-11-16 23:31:16 +0000

    Add project details

---
+++ src/screens/Project/DetailsProject.tsx
@@ -0,0 +1,53 @@
+import { Card } from "../../components/Card";
+import { Container } from "../../components/Container";
+import { Heading } from "../../components/Heading";
+import { Navbar } from "../../components/Navbar";
+import { Text } from "../../components/Text";
+import { TableListUser } from "../RH/User/TableListUser";
+
+export function DetailsProject() {
+    var project =
+    {
+        id: "d5d6be6e-d332-41be-b601-8bd2ae4e6935", name: 'Análise e Desenvolvimento de Sistemas',
+        employees: [
+            { id: "75b6a6f8-88fb-4642-bd79-60cd4865691c", name: "Paulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo AndradePaulo Andrade", role: "Funcionario", department: "RH" },
+            { id: "ad24ccc1-efe2-4b17-8bbe-05a4e730b12a", name: "Marcelo Cabelinho", role: "Funcionario", department: "Financeiro" },
+            { id: "f5537fec-16f2-452b-a6e4-4e60d36f5686", name: "Marcos ", role: "Chefe", department: "Financeiro" },
+            { id: "3f506407-4638-4bd0-abb9-d2bcf415239c", name: "Maria Marieta", role: "Chefe", department: "ADM" },
+            { id: "78e1efba-f2d1-473c-b656-8f70c9e8a94c", name: "Laura ", role: "Funcionario", department: "Almoxarifado" },
+            { id: "277dc916-e89e-444d-ac02-3bab065e488d", name: "Luan ", role: "Supervisor", department: "ADM" },
+            { id: "7ab0ac84-4500-4d68-907c-1ef5ed21d9ad", name: "Franciele ", role: "Supervisor", department: "RH" },
+        ]
+    };
+
+    return (
+        <Container>
+            <Navbar />
+            <div className="w-full flex py-6 p-24 max-md:p-6 flex-col">
+                <Heading size="lg">Detalhes Projeto</Heading>
+                <Card className="w-full !p-0">
+                    <div className="flex h-36 max-sm:flex-col max-sm:h-fit">
+                        <div className="flex flex-col md:w-4/5 p-4 justify-evenly">
+                            <div className="w-full flex items-center justify-between">
+                                <Text className="!text-gray-900 font-semibold">{project.name}</Text>
+                            </div>
+
+                        </div>
+                        <div className="bg-gray-300 w-full flex flex-col justify-evenly p-4">
+                            <div>
+                                <Text className="!text-gray-900 font-semibold">Total de funcionários: </Text>
+                                <Text className="!text-gray-900">{project.employees.length}</Text>
+                            </div>
+
+                        </div>
+                    </div>
+                </Card>
+
+                <div className="mt-10">
+                    <Heading>Funcionários nesse Curso</Heading>
+                    <TableListUser data={project.employees} />
+                </div>
+            </div>
+        </Container>
+    )
+}
