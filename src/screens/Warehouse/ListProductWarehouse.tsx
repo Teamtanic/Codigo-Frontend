e734236b548f71b325f7644931d1a3be2664ec2d
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-11-16 23:38:20 +0000

    Add product modals

--- src/screens/Warehouse/ListProductWarehouse.tsx
+++ src/screens/Warehouse/ListProductWarehouse.tsx
@@ -4,8 +4,8 @@ import { Heading } from "../../components/Heading";
 import { Navbar } from "../../components/Navbar";
 import { TextInput } from "../../components/TextInput";
 import { MagnifyingGlass } from 'phosphor-react';
-import { CardModal } from "../../components/CardModal";
 import { TableListProductWarehouse } from "./TableListProductWarehouse";
+import { ProductAddModal } from "./ProductAddModal";
 
 export function ListProductWarehouse() {
     var warehouse = [
@@ -31,7 +31,7 @@ export function ListProductWarehouse() {
 
                 <div className="w-full mt-3">
                     <Card className="w-full py-3 px-4">
-                        <TextInput.Root colorLabel="text-gray-900">
+                        <TextInput.Root labelStyle="text-gray-900">
                             <TextInput.Icon>
                                 <MagnifyingGlass />
                             </TextInput.Icon>
@@ -42,22 +42,7 @@ export function ListProductWarehouse() {
                     <div className="mt-10">
                         <TableListProductWarehouse data={warehouseDTO} />
 
-                        <CardModal title="Cadastro de Produto" action="Adicionar" triggerStyle="fixed w-fit bottom-5 right-8 rounded shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] !text-gray-100">
-                            <div className="flex flex-col w-full max-md:px-12 md:px-24 mb-12 gap-4">
-                                <TextInput.Root labelFor="product" labelText="Produto">
-                                    <TextInput.Input id="product" type="text" placeholder="Digite o produto..." />
-                                </TextInput.Root>
-                                <TextInput.Root labelFor="quantity" labelText="Quantidade">
-                                    <TextInput.Input id="quantity" type="number" min="0" placeholder="Digite a quantidade..." />
-                                </TextInput.Root>
-                                <TextInput.Root labelFor="supplier" labelText="Fornecedor">
-                                    <TextInput.Input id="supplier" type="text" placeholder="Informe o fornecedor..." />
-                                </TextInput.Root>
-                                <TextInput.Root labelFor="price" labelText="Preço">
-                                    <TextInput.Input id="price" type="text" placeholder="Informe o preço..." />
-                                </TextInput.Root>
-                            </div>
-                        </CardModal>
+                        <ProductAddModal />
                     </div>
                 </div>
             </div>
