f4a1f910ee4efbf3eacc1bcaad9db22ff9dec240
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-11-16 23:32:04 +0000

    Add options to table

--- src/screens/Project/TableListProject.tsx
+++ src/screens/Project/TableListProject.tsx
@@ -1,22 +1,39 @@
 import { CopiableText } from "../../components/CopiableText";
+import { ModalOptions } from "../../components/OptionsMenu";
 import { Column, Table } from "../../components/Table";
+import { ProjectEditModal } from "./ProjectEditModal";
 
 export interface ProjectProps {
     id: string,
-    description: string
+    description: string,
+    customer: string,
+    status: string
 }
 
-export function TableListProject({ data }: {data: ProjectProps[]}){
+export interface TableListProps {
+    hasOptions?: boolean;
+}
+
+export function TableListProject({ data, hasOptions = true }: {data: ProjectProps[]} & TableListProps){
     var columns : Column<ProjectProps>[] = [
         { key: 'id', title: 'ID', 
         render: ({id}) => 
             <CopiableText id={true} text={id}></CopiableText>,
-        width: '10'
+        width: 'w-10'
     },
         { key: 'description', title: 'Projeto' },
+        { key: 'customer', title: 'Cliente' },
+        { key: 'status', title: 'Situação' }
     ];
 
+    var options: ModalOptions[] = [
+        {
+            key: 'Editar', children:
+                <ProjectEditModal />
+        }
+    ]
+
     return (
-        <Table data={data} columns={columns} />
+        <Table data={data} columns={columns} menu={hasOptions} options={options}/>
     )
 }
