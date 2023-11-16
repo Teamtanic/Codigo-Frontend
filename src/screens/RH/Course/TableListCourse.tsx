5944a363c36f2e43f5d20bdb8fcd0855d75032fe
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-11-16 23:34:33 +0000

    Refactor table

--- src/screens/RH/Course/TableListCourse.tsx
+++ src/screens/RH/Course/TableListCourse.tsx
@@ -1,4 +1,6 @@
+import { ModalOptions } from "../../../components/OptionsMenu";
 import { Column, Table } from "../../../components/Table";
+import { CourseEditModal } from "./CourseEditModal";
 
 export interface CourseProps {
     id: string,
@@ -12,7 +14,14 @@ export function TableListCourse({ data }: {data : CourseProps[]}) {
         { key: 'employees', title: 'Funcionários' },
     ];
 
+    var options: ModalOptions[] = [
+        {
+            key: 'Editar', children:
+                <CourseEditModal />
+        }
+    ]
+
     return (
-        <Table data={data} columns={columns} />
+        <Table data={data} columns={columns} options={options} />
     )
 }
