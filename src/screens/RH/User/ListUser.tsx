130f5709df78520b6a6afdaf7196f4a520728fab
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-11-16 23:36:26 +0000

    Fix wrong reference

--- src/screens/RH/User/ListUser.tsx
+++ src/screens/RH/User/ListUser.tsx
@@ -26,7 +26,7 @@ export function ListUser() {
 
                 <div className="w-full mt-3">
                     <Card className="w-full py-3 px-4">
-                        <TextInput.Root colorLabel="text-gray-900">
+                        <TextInput.Root labelStyle="text-gray-900">
                             <TextInput.Icon>
                                 <MagnifyingGlass />
                             </TextInput.Icon>
