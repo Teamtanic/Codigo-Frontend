415b66aae27d951feb2b7e100d5b54557e72b659
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-11-16 23:26:40 +0000

    Fix unused style

--- src/screens/Financial/Bank/BankEditModal.tsx
+++ src/screens/Financial/Bank/BankEditModal.tsx
@@ -3,7 +3,7 @@ import { TextInput } from "../../../components/TextInput";
 
 export function BankEditModal(){
     return (
-        <CardModal title="Editar Banco" action="Editar Banco" triggerStyle="!w-full !bg-transparent !shadow-none hover:!bg-gray-200 hover:dark:!bg-gray-600 bottom-3 !text-gray-100 !font-normal">
+        <CardModal title="Editar Banco" action="Editar Banco" triggerStyle="!w-full !bg-transparent !shadow-none hover:!bg-gray-200 hover:dark:!bg-gray-600 bottom-3 !font-normal">
             <div className="flex flex-col w-full max-md:px-12 md:px-24 mb-12 gap-4">
                 <TextInput.Root labelFor="name" labelText="Nome">
                     <TextInput.Input id="name" type="text" placeholder="Digite o banco..." />
