67f8b22694fd61d1e86b415074bb1d56f46e827b
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-11-18 18:57:46 +0000

    Add defined nav link

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,16 +1,91 @@
-import { Link } from "react-router-dom"; // Importe o Link para criar links internos da sua aplicação
-import { Text } from "./Text";
-import { CaretLeft, CaretRight, Moon } from "phosphor-react";
-import { useEffect, useState } from "react";
+import { CaretLeft, CaretRight } from "phosphor-react";
+import { ReactNode, useEffect, useState } from "react";
 import { Button } from "./Button";
 import ThemeToggle from "./ThemeToggle";
+import Accordion from "./Accordion";
+import { NavLink } from "./NavLink";
+import { Text } from "./Text";
+
+
+export interface MenuItemsProps {
+  label: string,
+  subitens?: Array<ReactNode>
+}
 
 export interface SidebarMenuProps {
   sideBarVisible?: boolean;
-  menuItems: Array<{ label: string; link: string, notify?: number; }>;
 }
 
-export function SidebarMenu({ menuItems, sideBarVisible = false }: SidebarMenuProps) {
+const menuItems = [
+  {
+    label: "RH",
+    subitens: [
+      <NavLink link='/empresas'>
+        <Text size='sm'>Empresas</Text>
+      </NavLink>,
+      <NavLink link='/usuarios'>
+        <Text size='sm'>Usuários</Text>
+      </NavLink>,
+      <NavLink link='/autoridades'>
+        <Text size='sm'>Autoridades</Text>
+      </NavLink>,
+      <NavLink link='/cursos'>
+        <Text size='sm'>Cursos</Text>
+      </NavLink>,
+      <NavLink link='/departamentos'>
+        <Text size='sm'>Departamentos</Text>
+      </NavLink>,
+      <NavLink link='/cargos'>
+        <Text size='sm'>Cargos</Text>
+      </NavLink>,
+    ]
+  },
+
+  {
+    label: "Almoxarifado",
+    subitens: [
+      <NavLink key='item-1' link='/almoxarifado'>
+        <Text size='sm'>Produtos</Text>
+      </NavLink>,
+    ]
+  },
+
+  {
+    label: "Projetos",
+    subitens: [
+      <NavLink key='item-1' link='/projetos'>
+        <Text size='sm'>Projetos</Text>
+      </NavLink>,
+      <NavLink key='item-1' link='/ofertas'>
+        <Text size='sm'>Ofertas</Text>
+      </NavLink>,
+    ]
+  },
+
+  {
+    label: "Financeiro",
+    subitens: [
+      <NavLink key='item-1' link='/transacoes'>
+        <Text size='sm'>Transações</Text>
+      </NavLink>,
+      <NavLink key='item-1' link='/bancos'>
+        <Text size='sm'>Bancos</Text>
+      </NavLink>,
+    ]
+  },
+
+  {
+    label: "Documentos",
+    subitens: [
+      <NavLink key='item-1' link='/documentos'>
+        <Text size='sm'>Documentos</Text>
+      </NavLink>,
+    ]
+  },
+];
+
+
+export function SidebarMenu({ sideBarVisible = false }: SidebarMenuProps) {
   const [isSidebarVisible, setSidebarVisible] = useState(false);
 
   const toggleSidebar = () => {
@@ -26,41 +101,31 @@ export function SidebarMenu({ menuItems, sideBarVisible = false }: SidebarMenuPr
     : 'w-0 transition-all ease-in-out duration-200';
 
   return (
-    <div className="flex h-screen fixed overflow-y-auto z-40">
-      <aside className={` bg-gray-100 dark:bg-gray-800 ${sidebarClasses} md:border-r-4 border-r-gray-200/20 dark:border-r-gray-800/20`}>
-        <div className="flex flex-col h-full">
-          <nav className={`h-full mb-44 px-4 py-4 overflow-x-hidden ${!isSidebarVisible ? "overflow-y-hidden" : "overflow-y-auto"}`}>
-            {menuItems.map((item, index) => (
-              <Link
-                to={item.link}
-                key={index}
-                className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${!isSidebarVisible ? "hidden" : ""
-                  }`}
-              >
-                <Text className="flex-1 ml-3 whitespace-nowrap">{item.label}</Text>
-                {
-                  item.notify ? <Text className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-emerald-800 bg-green-300 rounded-full dark:bg-green-500 dark:text-emerald-900">{item.notify}</Text> : ""
-                }
-              </Link>
-            ))}
-            <div className={`fixed bottom-3 w-52 ${!isSidebarVisible ? "hidden" : ""}`}>
 
-              <ThemeToggle />
-            </div>
-          </nav>
-        </div>
-      </aside>
+      <div className="flex h-screen overflow-y-auto fixed z-40">
 
-      <div className="flex items-center max-md:hidden" >
-        <div onClick={toggleSidebar}>
-          <Button icon={isSidebarVisible ?
-            <CaretLeft size={32} className="text-gray-900 dark:text-gray-100" />
-            :
-            <CaretRight size={32} className="text-gray-900 dark:text-gray-100" />
-          } className="border-r-gray-200/20 bg-gray-200 hover:bg-gray-100  dark:bg-gray-800 dark:hover:bg-gray-600 rounded-l-none focus:ring-0 ">
-          </Button>
+        <aside className={` bg-gray-100 dark:bg-gray-800 ${sidebarClasses} md:border-r-4 border-r-gray-200/20 dark:border-r-gray-800/20`}>
+          <div className="flex flex-col h-full">
+            <nav className={`h-full mb-44 px-4 py-4 overflow-x-hidden ${!isSidebarVisible ? "overflow-y-hidden" : "overflow-y-auto"}`}>
+              <Accordion className={`${!isSidebarVisible ? 'hidden' : ''}`} items={menuItems} />
+              <div className={`fixed bottom-3 w-52 ${!isSidebarVisible ? "hidden" : ""}`}>
+                <ThemeToggle />
+              </div>
+            </nav>
+          </div>
+        </aside>
+
+        <div className="flex items-center max-md:hidden" >
+          <div onClick={toggleSidebar}>
+            <Button icon={isSidebarVisible ?
+              <CaretLeft size={32} className="text-gray-900 dark:text-gray-100" />
+              :
+              <CaretRight size={32} className="text-gray-900 dark:text-gray-100" />
+            } className="border-r-gray-200/20 bg-gray-200 hover:bg-gray-100  dark:bg-gray-800 dark:hover:bg-gray-600 rounded-l-none focus:ring-0 ">
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+
   );
 }
