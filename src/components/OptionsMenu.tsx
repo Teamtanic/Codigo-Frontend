6a8c4d400835d1c461701a9a41ea3dc46f9ea4ab
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2023-11-22 15:42:58 +0000

    Refactor

--- src/components/OptionsMenu.tsx
+++ src/components/OptionsMenu.tsx
@@ -1,60 +1,58 @@
-import { Menu, Transition } from "@headlessui/react"
-import { Button } from "./Button"
-import { DotsThreeOutlineVertical } from "phosphor-react"
-import { Text } from "./Text"
-import React, { Fragment } from "react"
-import { CardModal } from "./CardModal"
+import { Button } from "./Button";
+import { DotsThreeOutlineVertical } from "phosphor-react";
+import React from "react";
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { CardModal } from "./CardModal";
+import { Text } from "./Text";
 
 export interface OptionsMenu {
-    options: ModalOptions[];
+  options: ModalOptions[];
 }
 
 export interface ModalOptions {
-    key: string;
-    children: React.ReactNode;
+  key: string;
+  children: React.ReactNode;
 }
 
-export function OptionsMenu({ options }: OptionsMenu) {
 
-    return (
-        <Menu as="div" className="relative w-fit mr-0 ml-auto">
-            <Menu.Button>
-                <Button className="hover:!bg-gray-200 !h-full !bg-transparent !p-1 !w-fit rounded-full "
-                    title="Opções">
-                    <DotsThreeOutlineVertical className="!text-gray-800 truncate ml-auto mr-0" size={32} />
-                </Button>
-            </Menu.Button>
+export const OptionsMenu = ({ options }: OptionsMenu) => {
 
-            <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-            >
+  return (
+    <DropdownMenu.Root >
+      <DropdownMenu.Trigger >
+        <Button
+          className="hover:!bg-gray-200 !h-full !bg-transparent !p-1 !w-fit rounded-full"
+          title="Opções"
+        >
+          <DotsThreeOutlineVertical
+            className="!text-gray-800 truncate ml-auto mr-0"
+            size={32}
+          />
+        </Button>
+      </DropdownMenu.Trigger>
 
-                <Menu.Items className={`absolute z-30 right-0 bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600 `} id="dropdown-options">
-                    {options.map(option => {
-                        return (
-                            <Menu.Item key={option.key}>
-                                {option.children}
-                            </Menu.Item>
-                        )
-                    })}
+      <DropdownMenu.Portal >
 
-                    <Menu.Item key={'delete'}>
-                        <CardModal title="Deletar" action="Deletar" buttonStyle="bg-red-700 hover:bg-red-600" triggerStyle="!w-full !bg-transparent !shadow-none hover:!bg-gray-200 hover:dark:!bg-gray-600 bottom-3 !font-normal">
-                            <div className="flex flex-col items-center w-full max-md:px-12 md:px-24 mb-6 gap-4">
-                                <div>
-                                    <Text>Você tem certeza que deseja excluir este item?</Text>
-                                </div>
-                            </div>
-                        </CardModal>
-                    </Menu.Item>
-                </Menu.Items>
-            </Transition>
-        </Menu>
-    )
-}
+        <DropdownMenu.Content 
+          className=" z-30 right-0 bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600 "
+        >
+          {options.map((option) => (
+            option.children
+          ))}
+
+          <CardModal optionsTrigger title="Deletar" action="Deletar" triggerStyle="!w-full !bg-transparent !shadow-none hover:!bg-gray-200 hover:dark:!bg-gray-600 bottom-3 !font-normal">
+            <div className="flex flex-col items-center w-full max-md:px-12 md:px-24 mb-6 gap-4">
+              <div>
+                <Text>Você tem certeza que deseja excluir este item?</Text>
+              </div>
+              <Button className={`rounded-md z-50 !w-fit h-12 translate-y-16 ml-auto mr-0 bg-red-700 hover:bg-red-600`} textSize="sm" textStyle="text-gray-100">
+                Deletar
+              </Button>
+            </div>
+          </CardModal>
+          <DropdownMenu.Arrow className="fill-gray-700" />
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
+};
