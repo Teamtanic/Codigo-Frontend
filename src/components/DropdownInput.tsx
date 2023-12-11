7b2391979faab821dc92af1dc5a8a87828b5fa36
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2023-12-11 17:31:05 -0300

    Return id

--- src/components/DropdownInput.tsx
+++ src/components/DropdownInput.tsx
@@ -1,68 +1,94 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react'
 import { TextInput } from './TextInput'
+import { searchBankAccount } from '../services/BankAccount/apiService'
+import { BankAccountResponse } from '../services/BankAccount/types'
+import { Text } from './Text'
 
 export interface DropdownInputProps {
-  labelText?: string
-  labelFor?: string
+  labelText: string
+  labelFor: string
+  placeholder?: string
   className?: string
+  error?: string
   labelStyle?: string
+  inputValue: string
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 function DropdownInput({
   labelText,
   labelFor,
+  placeholder = '',
   className = '',
-  labelStyle = ''
+  error,
+  labelStyle = '',
+  inputValue = '',
+  onChange
 }: DropdownInputProps) {
-  const [inputValue, setInputValue] = useState('')
-  const [dropdownOptions, setDropdownOptions] = useState<string[]>([])
+  const [selectedOption, setSelectedOption] =
+    useState<BankAccountResponse | null>(null)
+  const [dropdownOptions, setDropdownOptions] = useState<BankAccountResponse[]>(
+    []
+  )
+  const [showDropdown, setShowDropdown] = useState(false)
 
   useEffect(() => {
-    // Função para buscar dados do servidor usando Axios
     const fetchData = async () => {
       try {
-        const response = await axios.get('/bancos', {
-          params: { searchTerm: inputValue }
-        })
-        setDropdownOptions(response.data)
+        const response = await searchBankAccount(inputValue)
+        setDropdownOptions(response.data.content.slice(0, 3))
       } catch (error) {
         console.error('Erro ao buscar dados:', error)
       }
     }
 
-    // Chama a função para buscar dados sempre que o valor do input muda
     fetchData()
   }, [inputValue])
 
+  const handleOptionClick = (option: BankAccountResponse) => {
+    setShowDropdown(false)
+    setSelectedOption(option)
+    onChange({
+      target: {
+        value: option.id
+      }
+    } as React.ChangeEvent<HTMLInputElement>)
+  }
+
   return (
-    <TextInput.Root
-      labelFor={labelFor}
-      labelText={labelText}
-      className={className}
-      labelStyle={labelStyle}
-    >
-      <div className="relative">
+    <>
+      <TextInput.Root
+        labelFor={labelFor}
+        labelText={labelText}
+        className={className}
+        labelStyle={labelStyle}
+        error={error}
+      >
         <TextInput.Input
           type="text"
-          value={inputValue}
-          onChange={e => setInputValue(e.target.value)}
+          placeholder={placeholder}
+          value={selectedOption?.name || inputValue}
+          onChange={e => {
+            setSelectedOption(null)
+            onChange(e)
+          }}
+          onFocus={() => setShowDropdown(true)}
         />
-        {dropdownOptions.length > 0 && (
-          <div className="absolute top-full left-0 w-full bg-white border border-gray-300 shadow-md rounded mt-2">
-            {dropdownOptions.map(option => (
-              <div
-                key={option}
-                className="py-2 px-4 cursor-pointer hover:bg-gray-100"
-                onClick={() => setInputValue(option)}
-              >
-                {option}
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </TextInput.Root>
+      </TextInput.Root>
+      {showDropdown && dropdownOptions.length > 0 && (
+        <div className="relative bg-white border border-gray-300 shadow-md rounded mt-2">
+          {dropdownOptions.map(option => (
+            <div
+              key={option.id}
+              className="py-2 px-4 cursor-pointer hover:bg-gray-100"
+              onClick={() => handleOptionClick(option)}
+            >
+              <Text className="text-gray-900">{option.name}</Text>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   )
 }
 
