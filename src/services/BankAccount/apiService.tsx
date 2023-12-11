2358f4adf929dc7930b2d5aaf6726a915f777f1b
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2023-12-11 17:33:08 -0300

    Paginate

--- src/services/BankAccount/apiService.tsx
+++ src/services/BankAccount/apiService.tsx
@@ -5,8 +5,13 @@ import {
   BankAccountResponsePaginate
 } from './types'
 
-export const getAllBankAccounts = () =>
-  api.get<BankAccountResponsePaginate>('/bancos')
+export const getAllBankAccounts = (page: number, size?: number) =>
+  api.get<BankAccountResponsePaginate>('/bancos', { params: { page, size } })
 
 export const createBankAccount = (bankAccountData: BankAccountCreateRequest) =>
   api.post<BankAccountResponse>('/bancos', bankAccountData)
+
+export const searchBankAccount = (name: string, page = 0, size = 3) =>
+  api.get<BankAccountResponsePaginate>('/bancos/pesquisa', {
+    params: { name, page, size }
+  })
