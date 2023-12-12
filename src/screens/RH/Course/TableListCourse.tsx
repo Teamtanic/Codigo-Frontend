1f23949d764d598202f94f571474d76037e684e1
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2023-12-12 14:41:11 -0300

    Add edit delete

--- src/screens/RH/Course/TableListCourse.tsx
+++ src/screens/RH/Course/TableListCourse.tsx
@@ -1,10 +1,20 @@
 import { ModalOptions } from '../../../components/OptionsMenu'
 import { Column, Table } from '../../../components/Table'
 import { Text } from '../../../components/Text'
+import { deleteCourse } from '../../../services/Course/apiService'
 import { CourseResponse } from '../../../services/Course/types'
 import { CourseModal } from './CourseModal'
 
 export function TableListCourse({ data }: { data: CourseResponse[] }) {
+  const handleDelete = async (record: CourseResponse) => {
+    try {
+      await deleteCourse(record.id)
+      window.location.reload()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   var columns: Column<CourseResponse>[] = [
     { key: 'name', title: 'Curso' }
     /*{ key: 'employees', title: 'Funcionários', render: ({employees}) => <Text className="!text-gray-900">{employees.length} {employees.length === 1 ? ' funcionário' : ' funcionários'}</Text>  },*/
@@ -14,10 +24,23 @@ export function TableListCourse({ data }: { data: CourseResponse[] }) {
     {
       key: 'Editar',
       children: (
-        <CourseModal title="Editar Curso" action="Editar" optionsTrigger />
+        <CourseModal
+          title="Editar Curso"
+          action="Editar"
+          optionsTrigger
+          mode="edit"
+        />
       )
     }
   ]
 
-  return <Table link="curso" data={data} columns={columns} options={options} />
+  return (
+    <Table
+      link="curso"
+      data={data}
+      columns={columns}
+      options={options}
+      onDelete={handleDelete}
+    />
+  )
 }
--- src/services/Course/apiService.tsx
+++ src/services/Course/apiService.tsx
@@ -2,10 +2,19 @@ import { api } from '../api'
 import {
   CourseCreateRequest,
   CourseResponse,
-  CourseResponsePaginate
+  CourseResponsePaginate,
+  CourseUpdateRequest
 } from './types'
 
 export const getAllCourses = () => api.get<CourseResponsePaginate>('/cursos')
 
 export const createCourse = (courseData: CourseCreateRequest) =>
   api.post<CourseResponse>('/cursos', courseData)
+
+export const editCourse = (courseData: CourseUpdateRequest, id: string) =>
+  api.patch<CourseResponse>(`/cursos/${id}`, courseData)
+
+export const deleteCourse = (id: string) => api.delete(`/cursos/${id}`)
+
+export const getCourseById = (id: string) =>
+  api.get<CourseResponse>(`/cursos/${id}`)
