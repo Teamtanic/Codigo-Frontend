8d9fadd969a6d190a71f264bfcc80b0bb8020e2f
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2023-12-13 01:58:46 -0300

    Refactor

--- src/components/DropdownInput.tsx
+++ src/components/DropdownInput.tsx
@@ -1,95 +1,106 @@
 import { useState, useEffect } from 'react'
-import { TextInput } from './TextInput'
-import { searchBankAccount } from '../services/BankAccount/apiService'
-import { BankAccountResponse } from '../services/BankAccount/types'
+import { Combobox } from '@headlessui/react'
 import { Text } from './Text'
 
-export interface DropdownInputProps {
-  labelText: string
-  labelFor: string
-  placeholder?: string
-  className?: string
-  error?: string
+interface ComboboxProps {
+  labelFor?: string
+  labelText?: string
   labelStyle?: string
-  inputValue: string
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  error?: string | undefined
+  inputValue: Record | null
+  placeholder?: string
+  onChange: (record: Record | null) => void
+  searchFunction: (query: string) => Promise<{ data: { content: Record[] } }>
+  disabled?: boolean
 }
 
-function DropdownInput({
-  labelText,
+interface Record {
+  value: string
+  label: string
+}
+
+export default function DropdownInput({
   labelFor,
-  placeholder = '',
-  className = '',
+  labelText,
+  labelStyle,
   error,
-  labelStyle = '',
-  inputValue = '',
-  onChange
-}: DropdownInputProps) {
-  const [selectedOption, setSelectedOption] =
-    useState<BankAccountResponse | null>(null)
-  const [dropdownOptions, setDropdownOptions] = useState<BankAccountResponse[]>(
-    []
-  )
-  const [showDropdown, setShowDropdown] = useState(false)
+  inputValue,
+  placeholder,
+  onChange,
+  disabled = false,
+  searchFunction
+}: ComboboxProps) {
+  const [query, setQuery] = useState('')
+  const [selectedRecord, setSelectedRecord] = useState<any>(inputValue)
+  const [records, setRecords] = useState<Record[]>([])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await searchBankAccount(inputValue)
-        setDropdownOptions(response.data.content.slice(0, 3))
-      } catch (error) {
-        console.error('Erro ao buscar dados:', error)
-      }
+  const fetchData = async (query: string) => {
+    try {
+      const response = await searchFunction(query)
+      setRecords(response.data.content.slice(0, 3))
+    } catch (error) {
+      console.error('Erro ao buscar dados:', error)
     }
-
-    fetchData()
-  }, [inputValue])
-
-  const handleOptionClick = (option: BankAccountResponse) => {
-    setShowDropdown(false)
-    setSelectedOption(option)
-    onChange({
-      target: {
-        value: option.id
-      }
-    } as React.ChangeEvent<HTMLInputElement>)
   }
 
+  useEffect(() => {
+    if (query !== '') {
+      fetchData(query)
+    }
+  }, [query, searchFunction])
+
+  const filteredRecords =
+    query === ''
+      ? records
+      : records.filter(record => {
+          return record.label.toLowerCase().includes(query.toLowerCase())
+        })
+
   return (
-    <>
-      <TextInput.Root
-        labelFor={labelFor}
-        labelText={labelText}
-        className={className}
-        labelStyle={labelStyle}
-        error={error}
+    <label htmlFor={labelFor} className="flex flex-col w-full">
+      <div className="flex gap-4">
+        <Text className={`${labelStyle}`}>{labelText}</Text>
+        <Text size="xm" className="text-red-600">
+          {error}
+        </Text>
+      </div>
+      <div
+        className={`font-poppins py-4 px-3 h-12 rounded bg-gray-200 w-full focus-within:ring-2 ring-emerald-800 flex items-center gap-3 ${
+          disabled ? 'bg-gray-400' : ''
+        }`}
       >
-        <TextInput.Input
-          type="text"
-          placeholder={placeholder}
-          value={selectedOption?.name || inputValue}
-          onChange={e => {
-            setSelectedOption(null)
-            onChange(e)
-          }}
-          onFocus={() => setShowDropdown(true)}
-        />
-      </TextInput.Root>
-      {showDropdown && dropdownOptions.length > 0 && (
-        <div className="relative bg-white border border-gray-300 shadow-md rounded mt-2">
-          {dropdownOptions.map(option => (
-            <div
-              key={option.id}
-              className="py-2 px-4 cursor-pointer hover:bg-gray-100"
-              onClick={() => handleOptionClick(option)}
-            >
-              <Text className="text-gray-900">{option.name}</Text>
-            </div>
-          ))}
-        </div>
-      )}
-    </>
+        <Combobox
+          value={selectedRecord}
+          by="id"
+          onChange={onChange}
+          disabled={disabled}
+        >
+          <Combobox.Input
+            placeholder={placeholder}
+            className={`text-gray-900 w-full bg-gray-200 outline-none ${
+              disabled ? 'bg-gray-400' : ''
+            }`}
+            onChange={event => setQuery(event.target.value)}
+            displayValue={record => record.label}
+          />
+          <Combobox.Options
+            className={'w-full bg-gray-100 rounded text-gray-900 '}
+          >
+            {filteredRecords.map(record => (
+              <Combobox.Option
+                className={
+                  'hover:bg-gray-300 rounded overflow-hidden hover:cursor-pointer px-6 py-2 '
+                }
+                key={record.value}
+                value={record.value}
+                onClick={() => setSelectedRecord(record)}
+              >
+                {record.label}
+              </Combobox.Option>
+            ))}
+          </Combobox.Options>
+        </Combobox>
+      </div>
+    </label>
   )
 }
-
-export default DropdownInput
